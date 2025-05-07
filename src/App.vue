9e403017a08adaa242f4845fd8bd4d2a9a3b23ca
Muhammad Pradityo <muhammadpradityo@student.uir.ac.id>
2025-05-07 17:44:28 +0700

    Tampilan awal dan untuk  menambahkan kegiatan

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="todo-container">
+    <h1>Daftar Kegiatan Saya</h1>
+    <div class="input-group">
+      <input type="text" v-model="newTask" placeholder="Tambahkan Kegiatan..." />
+      <button @click="addTask">Tambahkan Kegiatan</button>
+    </div>
+    <ul class="task-list">
+      <li v-for="(task, index) in tasks" :key="index">
+        {{ task.text }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const newTask = ref("");
+const tasks = ref([]);
+
+const addTask = () => {
+  if (newTask.value.trim()) {
+    tasks.value.push({ text: newTask.value });
+    newTask.value = "";
+  }
+};
+</script>
